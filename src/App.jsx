5072c0cb5d878027d484580e054c0a7224d1e670
Aminulhoque01 <aminuhoque53@gmail.com>
2026-01-13 16:29:44 +0600

    economist project stepup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+ 
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className="text-4xl font-bold text-red-500">
+      Tailwind is Working 🚀
+    </h1>
+      
+       
+    </>
+  )
+}
+
+export default App
